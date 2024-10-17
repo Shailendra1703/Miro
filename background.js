04610c480b62710dd25e44b29bd5cba5f299adf3
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2024-10-17 20:09:18 +0530

    fix: GPT Notifications

--- background.js
+++ background.js
@@ -1,24 +1,79 @@
+const CONVERSATION_URL = "https://chatgpt.com/backend-api/conversation";
+const RESPONSE_REQUESTED = "responseRequested";
+const RESPONSE_STREAMING = "responseStreaming";
+const RESPONSE_COMPLETED = "responseCompleted";
+const filters = {
+    urls: ["https://chatgpt.com/*", "https://chat.openai.com/*"],
+};
+
+function sendNotification(details) {
+    chrome.notifications.create({
+        type: "basic",
+        ...details,
+    });
+}
+
+function sendMessage(message) {
+    chrome.runtime.sendMessage({ message }, (response) => {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        } else {
+            console.log("Response received:", response);
+        }
+    });
+}
+
+function messageHandler(request, sender, sendResponse) {
+    // Notify user when response is streaming and completed
+    if (request.message === RESPONSE_STREAMING) {
+        console.log("Miro: Reponse is being streamed");
+        sendNotification({
+            iconUrl: "images/icon128.png",
+            title: "ChatGPT Response Streaming",
+            message: "ChatGPT is streaming its response.",
+        });
+    } else if (request.message === RESPONSE_COMPLETED) {
+        console.log("Miro: Response is completed");
+        sendNotification({
+            iconUrl: "images/icon128.png",
+            title: "ChatGPT Response Complete",
+            message: "ChatGPT has finished its response.",
+        });
+    } else if (request.message === RESPONSE_REQUESTED) {
+        console.log("Miro: Question asked by user");
+    } else {
+        console.error("Miro: Unrecognized message:", request.message);
+    }
+}
+
+// Notify when the extension is installed
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.notifications.create({
-    type: "basic",
-    iconUrl: "images/icon128.png",
-    title: "Test Notification",
-    message: "ChatGPT Notifier is Installed and Working!",
-  });
+    sendNotification({
+        iconUrl: "images/icon128.png",
+        title: "Test Notification",
+        message: "ChatGPT Notifier is Installed and Working!",
+    });
 });
 
-// Listener for runtime messages
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "responseComplete") {
-    console.log("Received message in background script");
-    chrome.notifications.create({
-      type: "basic",
-      iconUrl: "images/icon128.png",
-      title: "ChatGPT Response Complete",
-      message: "ChatGPT has finished its response.",
-    });
-    sendResponse({ status: "notification sent" });
-  } else {
-    sendResponse({ status: "unknown message" });
-  }
-});
+// Listening network request for conversation
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+        if (details.url === CONVERSATION_URL) {
+            messageHandler({ message: RESPONSE_REQUESTED });
+        }
+    },
+    filters,
+    ["requestHeaders"]
+);
+
+chrome.webRequest.onResponseStarted.addListener((details) => {
+    if (details.url === CONVERSATION_URL) {
+        messageHandler({ message: RESPONSE_STREAMING });
+    }
+}, filters);
+
+chrome.webRequest.onCompleted.addListener((details) => {
+    if (details.url === CONVERSATION_URL) {
+        messageHandler({ message: RESPONSE_COMPLETED });
+    }
+}, filters);
